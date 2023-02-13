dc17574a7b2db0bd490e1f95ebd02574f1633561
Istamovashakhzoda <117287001+Istamovashakhzoda@users.noreply.github.com>
2023-02-13 14:27:37 +0300

    Add files via upload

---
+++ masala-if/script.js
@@ -0,0 +1,260 @@
+"use script"
+
+//1-MASALA  
+/*let a=Number(prompt("Hafta kunlari"));
+let output;
+switch (day){
+  case 0:
+    output="Yakshanba"
+    break;
+    case 1:
+      output ="Dushanba"
+      break;
+      case 2:
+        output="Seshanba"
+        break;
+        case 3:
+          output="Chorshanba"
+          break;
+          case 4:
+          output="Payshanba"
+          break;
+          case 5:
+            output="Jum'a"
+            break;
+            case 6:
+              output="Shanba"
+              break;
+}
+alert(`${output}`);*/
+
+
+//2-MASALA
+/*let score =Number(prompt("Enter the score:"));
+
+
+let output;
+
+switch(score){
+  case 1:
+  output = "Yomon";
+  break;
+  case 2:
+    output = "Qoniqarsiz";
+    break;
+    case 3:
+      output = "Qoniqarli";
+      break;
+      case 4:
+        output = "Yaxshi";
+        break;
+        case 5:
+          output = "A'lo";
+          break;
+          default:
+            output = "Xato";
+}alert(`${output}`);*/
+
+//3-MASALA
+/*let season=Number(prompt("Your options:"));
+let output;
+
+switch(season){
+  case 1:
+    output = "Qish"
+    break;
+    case 2:
+    output = "Qish"
+    break;
+    case 3:
+    output = "Qish"
+    break;
+    case 4:
+    output = "Bahor"
+    break;
+    case 5:
+    output = "Bahor"
+    break;
+    case 6:
+    output = "Bahor"
+    break;
+    case 7:
+    output = "Yoz"
+    break;
+    case 8:
+    output = "Yoz"
+    break;
+    case 9:
+    output = "Yoz"
+    break;
+    case 10:
+    output = "Kuz"
+    break;
+    case 11:
+    output = "Kuz"
+    break;
+    case 12:
+    output = "Kuz"
+    break;
+}
+alert(`${output}`);*/
+
+//4-MASALA
+/*let a=Number(prompt("Months:"));
+let output;
+switch(a){
+  case 1:
+    output="31-kundan iborat"
+    break;
+    case 2:
+      output="28-29-kundan iborat"
+      break;
+      case 3:
+    output="31-kundan iborat"
+    break;
+    case 4:
+      output="30-kundan iborat"
+      break;
+      case 5:
+    output="31-kundan iborat"
+    break;
+    case 6:
+    output="30-kundan iborat"
+    break;
+    case 7:
+    output="31-kundan iborat"
+    break;
+    case 8:
+    output="31-kundan iborat"
+    break;
+    case 9:
+    output="30-kundan iborat"
+    break;
+    case 10:
+    output="31-kundan iborat"
+    break;
+    case 11:
+    output="30-kundan iborat"
+    break;
+    case 12:
+    output="31-kundan iborat"
+    break;
+}alert(`${output}`);*/
+
+//5-MASALA
+/*let a=Number(prompt("a"));
+let b=Number(prompt("b"));
+let c=Number(prompt("c"));
+
+
+let output;
+
+switch(c){
+  case "*":
+    output=a * b;
+    break;
+    case "/":
+      output= a/b;
+      break;
+      case "+":
+        output= a + b;
+        break;
+        case "-":
+          output = a-b;
+          break;
+}alert("output");*/
+
+//6-MASALA
+/*let a=Number(prompt("son"));
+let b=Number(prompt("tanlanggan=1-dm, 2-km,3-m, 4-mm ,5-cm"));
+let output;
+switch(b){
+  case 1:
+    output`${son*0.1}"metr"`;
+    break;
+    case 2:
+      output`${son*1000}"metr"`;
+      break;
+      case 3:
+        output`${son*1}"mert"`;
+        break;
+        case 4:
+          output`${son*0.001}"metr"`;
+          break;
+          case 5:
+            output`${son*0.01}"metr"`;
+            break;
+            default:
+              output="xato"
+}alert(`${output}`);*/
+
+
+//7-MASALA
+/*let a=Number(prompt("og'irlik"));
+let b=Number(prompt("tanlanggan=1-kg, 2-mg, 3-gmm, 4-tonna ,5-centener"));
+let output;
+switch(b){
+  case 1:
+    output`${son/100000}"kg"`;
+    break;
+    case 2:
+      output`${son/1000}"kg"`;
+      break;
+      case 3:
+        output`${son*1000}"kg"`;
+        break;
+        case 4:
+          output`${son*100}"kg"`
+          break;
+          default:
+            output="bunday qiymat yo'q"
+}alert(`${output}`);*/
+
+
+//8-MASALA
+/*let sana=Number(prompt("Kun"));
+let oy=Number(prompt("Oy"));
+let output;
+
+switch(oy){
+  case 1:
+   output`${"sana"} yanvar`;
+   break;
+   case 2:
+  output`${"sana"} fevral`;
+  break;
+  case 3:
+  output`${"sana"} mart`;
+  break;
+  case 4:
+ output`${"sana"} aprel`;
+  break;
+  case 5:
+  output`${"sana"} may`;
+  break;
+  case 6:
+output`${"sana"} iyun`;
+break;
+case 7:
+ output`${"sana"} iyul`;
+    break;
+                case 8:
+                  output`${"sana"} avgust`;
+                  break;
+                  case 9:
+                    output`${"sana"} sentabr`;
+                    break;
+                    case 10:
+                      output`${"sana"} oktabr`;
+                      break;
+                      case 11:
+                        output`${"sana"} noyabr`;
+                        break;
+                        case 12:
+                          output`${"sana"} dekabr`;
+                          break;
+}alert(`${output}`);*/
+
+//9-MASALA
+
+
